06af986e88be3a7ede4f7b518274ae962b65c246
remuria <aaa@aaa.com>
2021-07-02 12:21:48 +0900

    TopController,style/reset.cssの追加

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 
 Route::get('/', function () {
     return view('top');
 });
+*/
+
+Route::get('/', 'TopController@show'); 
