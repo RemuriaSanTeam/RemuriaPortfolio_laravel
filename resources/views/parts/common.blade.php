34d4f5a37f08790c53914623584e9f5f0134bf7f
remuria <aaa@aaa.com>
2021-07-04 19:04:26 +0900

    htmlのテンプレをviews.phpに組み込み中

--- resources/views/parts/common.blade.php
+++ resources/views/parts/common.blade.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
         @isset($title)
             <title>{{$title}} - Remustagram</title>
         @else
         <title>Remustagram</title>
         @endisset
-        <link rel="stylesheet" href="{{  asset('css/style.css') }}">
+        <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
+            rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+            rel="stylesheet" />
+        <link rel="stylesheet" href="{{  asset('css/styles.css') }}">
     </head>
     <body>
