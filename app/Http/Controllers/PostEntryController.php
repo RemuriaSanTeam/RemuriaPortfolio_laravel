f34d2b4485001eb04d221d4b598714a37cb251e5
remuria <aaa@aaa.com>
2021-07-05 21:29:24 +0900

    PostEntryControllerで発生してた問題の解決

--- app/Http/Controllers/PostEntryController.php
+++ app/Http/Controllers/PostEntryController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\PostEntry;
+use App\Models\PostEntry;
 
 class PostEntryController extends Controller
 {
     //
     function index(){
         //投稿一覧画面を表示
+        //ddでDBの中身確認したいときはmysqlを開いた状態にする(当たり前)
         dd(PostEntry::all());
     }
     function create(){
