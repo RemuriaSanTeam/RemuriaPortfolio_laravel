4ab6920734f7115242b44623239ffc8bbebde1d2
remuria <aaa@aaa.com>
2021-07-16 14:09:05 +0900

    cssテンプレート入れ替え

--- resources/views/parts/common.blade.php
+++ resources/views/parts/common.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
+        <meta name="description" content="RemuriaSanTeam" />
         <meta name="author" content="" />
         @isset($title)
             <title>{{$title}} - Remustagram</title>
