1ac8fab0cac527199c22b38d50be0a2092379447
remuria <aaa@aaa.com>
2021-07-16 15:38:21 +0900

    cssテンプレート入れ替え中

--- resources/views/parts/common.blade.php
+++ resources/views/parts/common.blade.php
@@ -11,13 +11,7 @@
         <title>Remustagram</title>
         @endisset
         <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
-            rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
-            rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <link rel="stylesheet" href="{{  asset('css/styles.css') }}">
     </head>
     <body>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,6 @@
-@include('parts.common')
-        @include('parts.header')
-        @include('parts.nav')
+@include('parts.common',["title"=>"投稿"])
+@include('parts.header')
+    @include('parts.nav')
         <!--<main>
             <h1>トップページ</h1>
             <p>ここには投稿者,画像,ストーリーバー,プロフィールを載せる
@@ -14,36 +14,51 @@
         @include("parts.form")
         @endcan
         <h2>記事一覧</h2>
+        <section class="py-5">
         @foreach($item_list as $item)
-            <div class="entry">
-                <h5>{{$item->title}} by {{$item->author}}</h5>
-                <div>
-                    <img src="{{Storage::url($item->image)}}" alt="投稿画像" width="120px">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="{{Storage::url($item->image)}}" alt="投稿画像" />
+                                <div class="entry card-body p-4">
+                                    <div class="text-center">
+                                        <h5 class="fw-bolder">{{$item->title}}</h5>
+                                        <h6>{{$item->author}}</h6>
+                                        {{nl2br(e($item->body))}}
+                                    </div>
+                                </div>
+                            </div>
+                            @if($item->user_id==Auth::user()->id)
+                            <!--削除ボタン-->
+                            <div class="card-footer p-4 pt-0 border-top-0">
+                                <form action='{{url('/remove')}}' method="POST">
+                                    @csrf
+                                    <input type='hidden' name='id' value='{{ $item->id }}'><!--ここでpost_entryのid要素を取得してる-->
+                                    <div class="card-footer p-4 pt-0 border-top-0">
+                                        <div class="text-center"><input type ='submit' class="btn-dell btn btn-danger mt-auto" value="削除"></div>
+                                    </div>
+                                </form>
+                            <!--編集ボタン-->
+                                    <div class="card-footer p-4 pt-0 border-top-0">
+                                        <a href="{{url('edit')}}?id={{$item->id}}" class="btn btn-info mt-auto">編集</a>
+                                    </div>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    {{nl2br(e($item->body))}}
-                </div>
-                {{--<p>ユーザーID:{{$item->user_id}}</p>--}}
-            </div>
-            @if($item->user_id==Auth::user()->id)
-            <!--削除ボタン-->
-            <form action='{{url('/remove')}}' method="POST">
-                @csrf
-                <input type='hidden' name='id' value='{{ $item->id }}'><!--ここでpost_entryのid要素を取得してる-->
-                <input type ='submit' class="btn-dell btn btn-danger" value="削除">
-            </form>
-            <!--編集ボタン-->
-            <a href="{{url('edit')}}?id={{$item->id}}" class="btn btn-info">編集</a>
-            @endif
+            {{--<p>ユーザーID:{{$item->user_id}}</p>--}}
         @endforeach
-
+    </section>
         @if(count($item_list)<1)
-        <p>投稿無いよ?</p>
+        <h5 class="fw-bolder">投稿ないよ？</h5>
         @endif
 
         @include('parts.footer')
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
         <script>
             $(function(){
                 $(".btn-dell").click(function(){
