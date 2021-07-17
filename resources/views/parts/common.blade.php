5422b00e2e15ddd659fc97715e0feb35d938a70f
remuria <aaa@aaa.com>
2021-07-17 15:03:59 +0900

    アイコン変更

--- resources/views/parts/common.blade.php
+++ resources/views/parts/common.blade.php
@@ -3,15 +3,15 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="RemuriaSanTeam" />
-        <meta name="author" content="" />
+        <meta name="description" content="Remustagram" />
+        <meta name="author" content="RemuriaSanTeam">
         @isset($title)
             <title>{{$title}} - Remustagram</title>
         @else
         <title>Remustagram</title>
         @endisset
         <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <link rel="stylesheet" href="{{  asset('css/styles.css') }}">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
     <body>
