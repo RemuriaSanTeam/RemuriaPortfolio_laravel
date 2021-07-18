8e3cbf68eea330f50f346c7ef4d2e69ab6c36e06
remuria <aaa@aaa.com>
2021-07-18 22:52:16 +0900

    from.blade.phpの整形

--- resources/views/parts/form.blade.php
+++ resources/views/parts/form.blade.php
@@ -14,18 +14,16 @@
         <label>Name</label><br/>
         <input type="text" name="author" value="" placeholder="名前💖"/>
     </div>-->
-    <div>
-        <label>Title</label><br/>
-        <input type="text" name="title" value="" placeholder="タイトル💖"/>
-    </div>
+    <input class="form-control mb-3" name="title" type="text" placeholder="タイトル💖" aria-label="Title">
     <!--画像アップロード-->
-    <div>
-        <label>Image</label><br/>
-        <input type="file" name="image" placeholder="アップロード💖"/>
+    <div class="mb-3">
+        <label for="formFileMultiple" class="form-label">Image</label>
+        <input class="form-control" type="file" name="image" id="formFileMultiple" placeholder="アップロード💖" multiple>
     </div>
-    <div>
-        <label>Body</label><br/>
-        <textarea name="body"></textarea>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Body</label>
+        <textarea name="body" class="form-control" id="exampleFormControlTextarea1" rows="5"
+            placeholder="何が言いたい？"></textarea>
     </div>
-        <input type="submit" value="投稿💖"/>
+    <input class="btn btn-primary mb-3" type="submit" value="投稿💖" />
 </form>
